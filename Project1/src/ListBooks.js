43fc886d8b073a81c80594c4244d7f1923f09b04
Abhishek Kumar Sharma <abhi243461@hotmail.com>
2020-05-11 19:07:19 +0530

    fixed: After change book shelf, books not rendering

--- Project1/src/ListBooks.js
+++ Project1/src/ListBooks.js
@@ -1,47 +1,7 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
-
-class RenderBooks extends Component {
-    state = {
-        shelf: this.props.shelf
-    }
-
-    render() {
-        const Books = this.props.Books
-        const shelf = this.state.shelf
-        return (
-            <ol className="books-grid">
-                {Books.map((book, index) => {
-                    if (book.shelf === shelf) {
-                        return (
-                            <li key={index}>
-                                <div className="book">
-                                    <div className="book-top">
-                                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                                        <div className="book-shelf-changer">
-                                            <select value={shelf} >
-                                                <option value="move" disabled>Move to...</option>
-                                                <option value="currentlyReading">Currently Reading</option>
-                                                <option value="wantToRead">Want to Read</option>
-                                                <option value="read">Read</option>
-                                                <option value="none">None</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                    <div className="book-title">{book.title}</div>
-                                    <div className="book-authors">{book.authors[0]}</div>
-                                </div>
-                            </li>
-                        )
-                    }
-                    return false
-                })
-                }
-            </ol>
-        )
-    }
-}
+import Shelf from './Shelf'
 
 class ListBooks extends Component {
     state = {
@@ -53,9 +13,20 @@ class ListBooks extends Component {
             this.setState(() => ({
                 Books
             }))
-            console.log(this.state.Books)
         })
     }
+
+    handleChange = (name, value) => {
+        let updateBook = this.state.Books[name]
+        BooksAPI.update(updateBook, value)
+            .then(() => {
+                updateBook.shelf = value
+                let addedBooks = this.state.Books.filter(book => book.id !== updateBook.id)
+                addedBooks.push(updateBook)
+                this.setState({ Books: addedBooks })
+            })
+    }
+
     render() {
         const Books = this.state.Books
         const shelf = ["currentlyReading", "wantToRead", "read"]
@@ -63,24 +34,24 @@ class ListBooks extends Component {
             <div className="list-books">
                 <div className="list-books-title">
                     <h1>MyReads</h1>
-                </div >
+                </div>
                 <div className="list-books-content">
                     <div>
                         <div className="bookshelf">
                             <h2 className="bookshelf-title">Currently Reading</h2>
                             <div className="bookshelf-books">
-                                <RenderBooks Books={Books} shelf={shelf[0]} />
+                                <Shelf handleChange={this.handleChange} Books={Books} shelf={shelf[0]} />
                             </div>
                         </div>
                         <div className="bookshelf">
                             <h2 className="bookshelf-title">Want To Read</h2>
                             <div className="bookshelf-books">
-                                <RenderBooks Books={Books} shelf={shelf[1]} />
+                                <Shelf handleChange={this.handleChange} Books={Books} shelf={shelf[1]} />
                             </div>
                         </div><div className="bookshelf">
                             <h2 className="bookshelf-title">Read</h2>
                             <div className="bookshelf-books">
-                                <RenderBooks Books={Books} shelf={shelf[2]} />
+                                <Shelf handleChange={this.handleChange} Books={Books} shelf={shelf[2]} />
                             </div>
                         </div>
                     </div>
---
+++ Project1/src/Shelf.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+
+class Shelf extends Component {
+    state = {
+        shelf: this.props.shelf,
+    }
+
+    render() {
+        const Books = this.props.Books
+        const shelf = this.state.shelf
+        return (
+            <ol className="books-grid">
+                {Books.map((book, index) => {
+                    if (book.shelf === shelf) {
+                        return (
+                            <li key={index}>
+                                <div className="book">
+                                    <div className="book-top">
+                                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                                        <div className="book-shelf-changer">
+                                            <select name={index} onChange={event => this.props.handleChange(event.target.name, event.target.value)} value={shelf} >
+                                                <option value="move" disabled>Move to...</option>
+                                                <option value="currentlyReading">Currently Reading</option>
+                                                <option value="wantToRead">Want to Read</option>
+                                                <option value="read">Read</option>
+                                                <option value="none">None</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div className="book-title">{book.title}</div>
+                                    <div className="book-authors">{book.authors[0]}</div>
+                                </div>
+                            </li>
+                        )
+                    }
+                    return false
+                })
+                }
+            </ol>
+        )
+    }
+}
+
+export default Shelf
