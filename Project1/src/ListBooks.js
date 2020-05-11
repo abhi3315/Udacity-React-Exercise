abb55c605ff690facc2905c970965c65af1799c3
Abhishek Kumar Sharma <abhi243461@hotmail.com>
2020-05-11 15:56:41 +0530

    refractor: Remove similar component states

--- Project1/src/ListBooks.js
+++ Project1/src/ListBooks.js
@@ -2,137 +2,43 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
 
-class CurrentlyReading extends Component {
+class RenderBooks extends Component {
     state = {
-        shelf: "currentlyReading"
+        shelf: this.props.shelf
     }
 
     render() {
         const Books = this.props.Books
         const shelf = this.state.shelf
         return (
-            <div className="bookshelf">
-                <h2 className="bookshelf-title">Currently Reading</h2>
-                <div className="bookshelf-books">
-                    <ol className="books-grid">
-                        {Books.map((book, index) => {
-                            if (book.shelf === shelf) {
-                                return (
-                                    <li key={index}>
-                                        <div className="book">
-                                            <div className="book-top">
-                                                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                                                <div className="book-shelf-changer">
-                                                    <select value={shelf} >
-                                                        <option value="move" disabled>Move to...</option>
-                                                        <option value="currentlyReading">Currently Reading</option>
-                                                        <option value="wantToRead">Want to Read</option>
-                                                        <option value="read">Read</option>
-                                                        <option value="none">None</option>
-                                                    </select>
-                                                </div>
-                                            </div>
-                                            <div className="book-title">{book.title}</div>
-                                            <div className="book-authors">{book.authors[0]}</div>
+            <ol className="books-grid">
+                {Books.map((book, index) => {
+                    if (book.shelf === shelf) {
+                        return (
+                            <li key={index}>
+                                <div className="book">
+                                    <div className="book-top">
+                                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                                        <div className="book-shelf-changer">
+                                            <select value={shelf} >
+                                                <option value="move" disabled>Move to...</option>
+                                                <option value="currentlyReading">Currently Reading</option>
+                                                <option value="wantToRead">Want to Read</option>
+                                                <option value="read">Read</option>
+                                                <option value="none">None</option>
+                                            </select>
                                         </div>
-                                    </li>
-                                )
-                            }
-                            return false
-                        })}
-                    </ol>
-                </div>
-            </div>
-        )
-    }
-}
-
-class WantToRead extends Component {
-    state = {
-        shelf: "wantToRead"
-    }
-
-    render() {
-        const Books = this.props.Books
-        const shelf = this.state.shelf
-        return (
-            <div className="bookshelf">
-                <h2 className="bookshelf-title">Currently Reading</h2>
-                <div className="bookshelf-books">
-                    <ol className="books-grid">
-                        {Books.map((book, index) => {
-                            if (book.shelf === shelf) {
-                                return (
-                                    <li key={index}>
-                                        <div className="book">
-                                            <div className="book-top">
-                                                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                                                <div className="book-shelf-changer">
-                                                    <select value={shelf}>
-                                                        <option value="move" disabled>Move to...</option>
-                                                        <option value="currentlyReading">Currently Reading</option>
-                                                        <option value="wantToRead">Want to Read</option>
-                                                        <option value="read">Read</option>
-                                                        <option value="none">None</option>
-                                                    </select>
-                                                </div>
-                                            </div>
-                                            <div className="book-title">{book.title}</div>
-                                            <div className="book-authors">{book.authors[0]}</div>
-                                        </div>
-                                    </li>
-                                )
-                            }
-                            return false
-                        })}
-                    </ol>
-                </div>
-            </div>
-        )
-    }
-}
-
-class Read extends Component {
-    state = {
-        shelf: "read"
-    }
-
-    render() {
-        const Books = this.props.Books
-        const shelf = this.state.shelf
-        return (
-            <div className="bookshelf">
-                <h2 className="bookshelf-title">Currently Reading</h2>
-                <div className="bookshelf-books">
-                    <ol className="books-grid">
-                        {Books.map((book, index) => {
-                            if (book.shelf === shelf) {
-                                return (
-                                    <li key={index}>
-                                        <div className="book">
-                                            <div className="book-top">
-                                                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                                                <div className="book-shelf-changer">
-                                                    <select value={shelf}>
-                                                        <option value="move" disabled>Move to...</option>
-                                                        <option value="currentlyReading">Currently Reading</option>
-                                                        <option value="wantToRead">Want to Read</option>
-                                                        <option value="read">Read</option>
-                                                        <option value="none">None</option>
-                                                    </select>
-                                                </div>
-                                            </div>
-                                            <div className="book-title">{book.title}</div>
-                                            <div className="book-authors">{book.authors[0]}</div>
-                                        </div>
-                                    </li>
-                                )
-                            }
-                            return false
-                        })}
-                    </ol>
-                </div>
-            </div>
+                                    </div>
+                                    <div className="book-title">{book.title}</div>
+                                    <div className="book-authors">{book.authors[0]}</div>
+                                </div>
+                            </li>
+                        )
+                    }
+                    return false
+                })
+                }
+            </ol>
         )
     }
 }
@@ -152,6 +58,7 @@ class ListBooks extends Component {
     }
     render() {
         const Books = this.state.Books
+        const shelf = ["currentlyReading", "wantToRead", "read"]
         return (
             <div className="list-books">
                 <div className="list-books-title">
@@ -159,9 +66,23 @@ class ListBooks extends Component {
                 </div >
                 <div className="list-books-content">
                     <div>
-                        <CurrentlyReading Books={Books} />
-                        <WantToRead Books={Books} />
-                        <Read Books={Books} />
+                        <div className="bookshelf">
+                            <h2 className="bookshelf-title">Currently Reading</h2>
+                            <div className="bookshelf-books">
+                                <RenderBooks Books={Books} shelf={shelf[0]} />
+                            </div>
+                        </div>
+                        <div className="bookshelf">
+                            <h2 className="bookshelf-title">Want To Read</h2>
+                            <div className="bookshelf-books">
+                                <RenderBooks Books={Books} shelf={shelf[1]} />
+                            </div>
+                        </div><div className="bookshelf">
+                            <h2 className="bookshelf-title">Read</h2>
+                            <div className="bookshelf-books">
+                                <RenderBooks Books={Books} shelf={shelf[2]} />
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div className="open-search">
