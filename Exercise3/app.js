f5b2254c50d75212b56b823b428dd29a55b26bdf
Abhishek Kumar Sharma <abhi243461@hotmail.com>
2020-05-13 19:00:45 +0530

    feat: Add app reducers

---
+++ Exercise3/app.js
@@ -0,0 +1,43 @@
+import createStore from './store'
+
+//Action types
+const ADD_TODO = 'ADD_TODO'
+const REMOVE_TODO = 'REMOVE_TODO'
+const TOGGLE_TODO = 'TOGGLE_TODO'
+const ADD_GOAL = 'ADD_GOAL'
+const REMOVE_GOAL = 'REMOVE_GOAL'
+
+//App functions/reducers
+function todos(state = [], action) {
+    switch (action.type) {
+        case ADD_TODO:
+            return state.concat([action.todo])
+        case REMOVE_TODO:
+            return state.filter(todo => todo.id !== action.id)
+        case TOGGLE_TODO:
+            return state.map(todo => todo.id !== action.id ? todo :
+                Object.assign({}, todo, { complete: !todo.complete }))
+        default:
+            return state
+    }
+}
+
+function goals(state = [], action) {
+    switch (action.type) {
+        case ADD_GOAL:
+            return state.concat([action.goal])
+        case REMOVE_GOAL:
+            return state.filter(goal => goal.id !== action.id)
+        default:
+            return state
+    }
+}
+
+function app(state = {}, action) {
+    return {
+        todos: todos(state.todos, action),
+        goals: goals(state.goals, action)
+    }
+}
+
+const store = createStore(app)
