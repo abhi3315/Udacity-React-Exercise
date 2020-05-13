25835e5e1bca6e4ddc4645e97a8ebbe0d53eb725
Abhishek Kumar Sharma <abhi243461@hotmail.com>
2020-05-13 19:14:00 +0530

    feat: Add action creator funtions

--- Exercise3/app.js
+++ Exercise3/app.js
@@ -7,6 +7,42 @@ const TOGGLE_TODO = 'TOGGLE_TODO'
 const ADD_GOAL = 'ADD_GOAL'
 const REMOVE_GOAL = 'REMOVE_GOAL'
 
+//Action Creator functions
+function addTodoAction(todo) {
+    return {
+        type: ADD_TODO,
+        todo
+    }
+}
+
+function removeTodoAction(id) {
+    return {
+        type: REMOVE_TODO,
+        id
+    }
+}
+
+function toggleTodoAction(id) {
+    return {
+        type: TOGGLE_TODO,
+        id
+    }
+}
+
+function addGoalAction(goal) {
+    return {
+        type: ADD_GOAL,
+        goal
+    }
+}
+
+function removeGoalAction(id) {
+    return {
+        type: REMOVE_GOAL,
+        id
+    }
+}
+
 //App functions/reducers
 function todos(state = [], action) {
     switch (action.type) {
