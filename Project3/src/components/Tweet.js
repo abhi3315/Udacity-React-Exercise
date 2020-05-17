f1b34a761c5d170853cfa899b7c820302fa85903
Abhishek Kumar Sharma <abhi243461@hotmail.com>
2020-05-17 14:17:56 +0530

    feat: Navbar, New tweet page, Reply to tweets

--- Project3/src/components/Tweet.js
+++ Project3/src/components/Tweet.js
@@ -3,10 +3,13 @@ import { connect } from 'react-redux'
 import { formatTweet, formatDate } from '../utils/helpers'
 import { TiArrowBackOutline, TiHeartOutline, TiHeartFullOutline } from 'react-icons/ti'
 import { handleToggleTweet } from '../actions/tweets'
+import { Link, withRouter } from 'react-router-dom'
 
 class Tweet extends Component {
     toParent = (e, id) => {
         e.preventDefault()
+
+        this.props.history.push(`/tweet/${id}`)
     }
 
     handleLike = (e) => {
@@ -30,10 +33,10 @@ class Tweet extends Component {
             )
         }
 
-        const { name, avatar, timestamp, text, hasLiked, likes, replies, parent } = tweet
+        const { name, avatar, timestamp, text, hasLiked, likes, replies, parent, id } = tweet
 
         return (
-            <div className="tweet">
+            <Link to={`/tweet/${id}`} className="tweet">
                 <img src={avatar} alt={`Avatar of ${name}`} className="avatar" />
 
                 <div className="tweet-info">
@@ -56,7 +59,7 @@ class Tweet extends Component {
                         <span>{likes !== 0 && likes}</span>
                     </div>
                 </div>
-            </div>
+            </Link>
         )
     }
 }
@@ -73,4 +76,4 @@ function mapStateToProps({ authedUser, users, tweets }, { id }) {
     }
 }
 
-export default connect(mapStateToProps)(Tweet)
+export default withRouter(connect(mapStateToProps)(Tweet))
